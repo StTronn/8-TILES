f6e49e0a273f271a748004115e390515034bdac5
rishav <rishav.21m@gmail.com>
2020-04-10 13:14:30 +0530

    Updates

--- precache-manifest.4af88d8b72fceb72ccfad8b465de5f25.js
+++ precache-manifest.4af88d8b72fceb72ccfad8b465de5f25.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "16cca652958f85ab0f6b87b9850ee60b",
+    "revision": "8e398b3e95b2b84c0c2ec475feab63b2",
     "url": "/8-TILES/index.html"
   },
   {
-    "revision": "8a823aa553eecce9f87f",
-    "url": "/8-TILES/static/css/main.56a6d031.chunk.css"
+    "revision": "ae4a5fa303fd5e292e81",
+    "url": "/8-TILES/static/css/main.0d623c6f.chunk.css"
   },
   {
     "revision": "1dcb7bb72d53320bd854",
     "url": "/8-TILES/static/js/2.bb08a7a2.chunk.js"
   },
   {
-    "revision": "8a823aa553eecce9f87f",
-    "url": "/8-TILES/static/js/main.033030b2.chunk.js"
+    "revision": "ae4a5fa303fd5e292e81",
+    "url": "/8-TILES/static/js/main.980e4dcd.chunk.js"
   },
   {
     "revision": "fafd2b81914d28f774b4",
