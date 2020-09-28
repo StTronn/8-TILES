fffbfe4fa106741920941ecc0394c9ab8d47e310
rishav <rishav.21m@gmail.com>
2020-09-28 15:34:57 +0530

    Updates

---
+++ precache-manifest.8cb8e38daa3b317cbc0b8625fbe32740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ca8f5e4a10f491487c6cb7c7b7de67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd7c83153984a1e4676e",
+    "url": "/static/css/main.0d623c6f.chunk.css"
+  },
+  {
+    "revision": "1dcb7bb72d53320bd854",
+    "url": "/static/js/2.bb08a7a2.chunk.js"
+  },
+  {
+    "revision": "bd7c83153984a1e4676e",
+    "url": "/static/js/main.af6d81c4.chunk.js"
+  },
+  {
+    "revision": "e590cc042d29092bbd12",
+    "url": "/static/js/runtime-main.a79bba7d.js"
+  }
+]);
