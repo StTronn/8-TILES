b512a84e25017d7b4428a858d8825adb17f4949a
rishav <rishav.21m@gmail.com>
2020-11-18 02:38:25 +0530

    --amend

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "cm-light": "#6f74dd",
+        "cm-main": "#3949ab",
+        "cm-dark": "#00227b",
+        "cm-dark-primary": "#283655",
+        "cm-dark-light": "#4d648d",
+        "cm-dark-dark": "#1e1f26",
+        "cm-bg-color": "#408697",
+      },
+      letterSpacing: {
+        "ls-02": "0.2em",
+      },
+    },
+  },
+};
